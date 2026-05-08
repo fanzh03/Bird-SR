40dede635c2443716ba7f587f212854d129428de
Xin Lu <166975157+xin1u@users.noreply.github.com>
2026-05-08 21:09:26 +0800

    Update test_wollava.py

--- test/test_wollava.py
+++ test/test_wollava.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 # 添加项目根目录到Python路径
-project_root = "/PATH/DiT4SR-main"
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 sys.path.insert(0, project_root)
 import glob
 import argparse
