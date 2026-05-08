192f3919d36377c4e1434fa4d407b2b870fa7e14
Xin Lu <166975157+xin1u@users.noreply.github.com>
2026-05-08 21:08:30 +0800

    Update CKPT_PTH.py

--- CKPT_PTH.py
+++ CKPT_PTH.py
@@ -1,2 +1,6 @@
-LLAVA_CLIP_PATH = '/PATH/DiT4SR-main/llava_ckpt/clip-vit-large-patch14-336'
-LLAVA_MODEL_PATH = '/PATH/DiT4SR-main/llava_ckpt/llava-v1.5-13b'
+import os
+
+_REPO_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+LLAVA_CLIP_PATH = os.path.join(_REPO_ROOT, 'llava_ckpt/clip-vit-large-patch14-336')
+LLAVA_MODEL_PATH = os.path.join(_REPO_ROOT, 'llava_ckpt/llava-v1.5-13b')
